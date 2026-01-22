ecfddcbb21a7d8dfca776143575e86264d321241
yubing0625 <yubing0625@gmail.com>
2026-01-22 19:36:28 +0800

    Add build and deployment workflow for Rust-based web game
    Introduce a GitHub Actions workflow to automate the build and deployment
    process for the project. The workflow includes steps for checking out the
    repository, setting up the Rust toolchain, building the project for
    `wasm32-unknown-unknown` target, and deploying the output to GitHub Pages.
    
    Additionally:
    - Add `macroquad` dependency for rendering and game development.
    - Update `main.rs` to implement a basic game window using `macroquad`.
    - Add an `index.html` file for running the game in a web browser.
    - Include a `Cargo.lock` file to track dependencies and maintain build
      consistency.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, new rpg!");
+use macroquad::prelude::*;
+use macroquad::ui::*;
+
+/// Window configuration
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "My Game".to_owned(),
+        window_width: 1920,
+        window_height: 1080,
+        // fullscreen: true,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        root_ui().label(None, "hello megaui");
+        if root_ui().button(None, "Push me") {
+            println!("pushed");
+        }
+
+        next_frame().await
+    }
 }

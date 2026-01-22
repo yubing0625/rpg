3a07ab3346c79c5172d737f19fbfe5598ae337fb
yubing0625 <yubing0625@gmail.com>
2026-01-22 20:54:37 +0800

    Updates macroquad entry point and comments out window config
    Replaces the custom `window_conf` function with a default string-based
    configuration for the macroquad entry point. The `window_conf` function
    is commented out, simplifying the window configuration and aligning
    the code with a default setup.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use macroquad::prelude::*;
 use macroquad::ui::*;
 
 /// Window configuration
-fn window_conf() -> Conf {
-    Conf {
-        window_title: "My Game".to_owned(),
-        window_width: 1920,
-        window_height: 1080,
-        // fullscreen: true,
-        ..Default::default()
-    }
-}
+// fn window_conf() -> Conf {
+//     Conf {
+//         window_title: "My Game".to_owned(),
+//         window_width: 1920,
+//         window_height: 1080,
+//         // fullscreen: true,
+//         ..Default::default()
+//     }
+// }
 
-#[macroquad::main(window_conf)]
+#[macroquad::main("rpg")]
 async fn main() {
     loop {
         clear_background(BLACK);

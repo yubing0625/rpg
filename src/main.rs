c7e6d33579228955417afa85588447b60a9fb073
yubing0625 <yubing0625@gmail.com>
2026-01-23 08:24:50 +0800

    Adds player movement with arrow keys
    Introduces functionality to move a circle representing the player
    on the screen using the arrow keys. The player can move in four
    directions: up, down, left, and right, by adjusting its x and y
    coordinates based on key presses.

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,32 @@ use macroquad::prelude::*;
 //     }
 // }
 
+
+
+
 #[macroquad::main("rpg")]
 async fn main() {
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() / 2.0;
+
     loop {
         clear_background(DARKPURPLE);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
-        draw_text("HELLO", 20.0, 20.0, 30.0, DARKGRAY);
+        if is_key_down(KeyCode::Right) {
+            x += 1.0;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= 1.0;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += 1.0;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= 1.0;
+        }
+
+        draw_circle(x, y, 16.0, YELLOW);
 
         next_frame().await
     }
